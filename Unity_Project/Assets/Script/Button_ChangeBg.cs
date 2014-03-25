00c064f0492b00c28d5f737172d121affb3832a2
liaoyg <liaoyg620@163.com>
2014-03-25 23:52:47 +0800

    your commit message

---
+++ Unity_Project/Assets/Script/Button_ChangeBg.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button_ChangeBg : MonoBehaviour {
+
+
+	public Transform target;
+	// Use this for initialization
+	int Bgposition = 0;
+	bool Startchange = false;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Startchange) {
+			SpringPosition sp = target.GetComponent<SpringPosition> ();
+			if(sp.target.x != Bgposition)
+			{
+				sp.target.Set (Bgposition, 0, 0);
+				sp.enabled = true;
+			}
+			else
+			{
+				Startchange = false;
+			}
+
+		}
+	}
+
+	void ChangeBackgroundInfo()
+	{
+		Bgposition = 640;
+		Startchange = true;
+	}
+	void ChangeBackgroundCreate()
+	{
+		Bgposition = 0;
+		Startchange = true;
+	}
+	void ChangeBackgroundReceive()
+	{
+		Bgposition = -640;
+		Startchange = true;
+	}
+}
